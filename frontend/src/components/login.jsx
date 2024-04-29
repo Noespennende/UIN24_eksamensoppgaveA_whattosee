01ec272132ac2bb8ee9133b2a35014da2995b739
M1kke95 <m1kke95@hotmail.com>
2024-04-29 11:36:33 +0200

    satt opp Router
    Satt opp router i app-jsx og i main.jsx

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,12 @@
+export default function Loginpage(){
+
+    return (
+        <>
+        <ul>
+        <button>navn</button>
+        <button>navn2</button>
+        <button>navn3</button>
+        </ul>
+        </>
+    )
+}
