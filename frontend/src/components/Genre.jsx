ed8965c3704141ff8b79c1331428ad4648f4edc8
Andreasberg89 <andber9@gmail.com>
2024-04-29 21:02:45 +0200

    movie api services

--- frontend/src/components/Genre.jsx
+++ frontend/src/components/Genre.jsx
@@ -9,23 +9,7 @@ export default function Genre(){
     const [film, setFilm] = useState()
 
     const getImdbTitle = async() => {
-        const url = 'https://moviesdatabase.p.rapidapi.com/titles/tt0086250';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'ee8f2cfc00msh51a950f97794d08p135048jsn974877703753',
-                'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-            }
-        };
-
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-            setMovie(result)
-        } catch (error) {
-            console.error(error);
-        }
+        fetchByImdbId("", setFilm)
         }
     
         useEffect(() => {
