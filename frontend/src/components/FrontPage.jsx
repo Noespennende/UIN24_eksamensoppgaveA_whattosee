e3182a374f56fd7fb8b202cb665098ea5e2b5a52
Tuva <tuva.tma@gmail.com>
2024-04-29 19:16:50 +0200

    Satt opp slug til bruker og ''fordelt'' oppsettet av siden

--- frontend/src/components/FrontPage.jsx
+++ frontend/src/components/FrontPage.jsx
@@ -1,14 +1,28 @@
 import NavBar from './NavBar'
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 
 
 export default function FrontPage() {
+    const { slug } = useParams()
+
+
     return (
         <>
             <NavBar></NavBar>
-            <h1>Hei, Bruker. Velkommen til forsiden under konstruksjon</h1>
+            <h1>Hei, {slug}. Velkommen til forsiden din!</h1>
             <Link to="/"><button>logout</button></Link>
+            <section id="skal_se">
+                <h3>Filmer jeg skal se!</h3>
+            </section>
+            <section id="film_ønskeliste">
+                <p>Disse filmene ligger i ønskelisten din:</p>
+            </section>
+            <article id="se_med">
+                <h3>Jeg skal se sammen med...</h3>
+                <ul>Liste med andre brukere her</ul>
+            </article>
         </>
     )
 }
