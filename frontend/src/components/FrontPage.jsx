458008ff3aef088d9280c511830675582a5fc54f
Tuva <tuva.tma@gmail.com>
2024-04-30 23:57:18 +0200

    Kan nå gå til Dashboard via Frontpage

--- frontend/src/components/FrontPage.jsx
+++ frontend/src/components/FrontPage.jsx
@@ -5,29 +5,29 @@ import { fetchUsers } from '../../sanity/services/loginServices'
 
 
 
-export default function FrontPage( {onLogout}) {
+export default function FrontPage({ onLogout }) {
     const { slug } = useParams()
     const [users, setUsers] = useState([])
     const loggedInUser = JSON.parse(localStorage.getItem('LoggedInUser'))
-   
+
 
     useEffect(() => {
         const fetchData = async () => {
             const allUsers = await fetchUsers();
             const usersFiltered = allUsers.filter(user => user.username !== loggedInUser)
-            setUsers(usersFiltered)   
+            setUsers(usersFiltered)
         }
         fetchData();
     }, [loggedInUser])
 
-  
-    const handlelogout =() =>{
+
+    const handlelogout = () => {
         onLogout()
     }
 
     return (
         <>
-            <NavBar LoggedInUser={loggedInUser}/>
+            <NavBar LoggedInUser={loggedInUser} />
             <h1>Hei, {slug}. Velkommen til forsiden din!</h1>
             <Link to="/"><button onClick={handlelogout}>logout</button></Link>
             <section id="skal_se">
@@ -38,10 +38,11 @@ export default function FrontPage( {onLogout}) {
                 <h3>Jeg skal se sammen med...</h3>
                 <ul>
                     {users.map((user, index) => (
-                        <li key={index}>{user.username}</li>
-                    ))}
+                        <li key={index}>
+                            <Link to={`/Dashboard/${user.username}`}>{user.username}</Link>
+                        </li>))}
                 </ul>
-            </article>
+            </article >
         </>
     )
 }
