8315562982936c6132b03cfbd8cfe3bc63f7acbc
M1kke95 <m1kke95@hotmail.com>
2024-04-30 21:25:55 +0200

    litt små endringer på forskjellige komponenter
    fikset handlelogout på frontpage, fjernet navbar på layout inntil videre for å unngå duplikat, la til links til forskjellige sider i navbaren

--- frontend/src/components/FrontPage.jsx
+++ frontend/src/components/FrontPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import NavBar from './NavBar'
-import { Link } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { fetchUsers } from '../../sanity/services/loginServices'
 
 
@@ -10,6 +9,7 @@ export default function FrontPage( {onLogout}) {
     const { slug } = useParams()
     const [users, setUsers] = useState([])
     const loggedInUser = JSON.parse(localStorage.getItem('LoggedInUser'))
+   
 
     useEffect(() => {
         const fetchData = async () => {
@@ -20,16 +20,16 @@ export default function FrontPage( {onLogout}) {
         fetchData();
     }, [loggedInUser])
 
-
-    const handleLogout = ()=> {
+  
+    const handlelogout =() =>{
         onLogout()
     }
 
     return (
         <>
-            <NavBar></NavBar>
+            <NavBar LoggedInUser={loggedInUser}/>
             <h1>Hei, {slug}. Velkommen til forsiden din!</h1>
-            <Link to="/"><button onClick={handleLogout}>logout</button></Link>
+            <Link to="/"><button onClick={handlelogout}>logout</button></Link>
             <section id="skal_se">
                 <h3>Filmer jeg skal se!</h3>
                 <p>Disse filmene ligger i ønskelisten din:</p>
