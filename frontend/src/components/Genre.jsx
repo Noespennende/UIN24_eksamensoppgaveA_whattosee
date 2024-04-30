4ee055137b970d3df0c4741cdb19383c56a9a04c
Andreasberg89 <andber9@gmail.com>
2024-04-30 19:11:22 +0200

    MovieCard og Genre henter nå informasjon fra Sanity og IMDB databasen

--- frontend/src/components/Genre.jsx
+++ frontend/src/components/Genre.jsx
@@ -8,42 +8,23 @@ import { fetchAllMovies, fetchMoviesByGenre } from "../../sanity/services/movieS
 
 export default function Genre(){
     const [movieList, setMovieList] = useState()
-    const [imdb, setImdb] = useState([])
     const {slug} = useParams()
     let movieCount = 0
 
     const getMovieData = async (slug) => {
         const sanityData = await fetchMoviesByGenre(slug)
-        sanityData?.map((movie) => fetchImdbData(movie.imdbid))
-        console.log(slug)
         setMovieList(sanityData)
         
     }
-
-    const fetchImdbData = async(movieId) => {
-        const url = `https://moviesdatabase.p.rapidapi.com/titles/${movieId}`;
-        try {
-            const response = await fetch(url, apiClient);
-            const result = await response.json();
-            imdb.push({
-                url: result.results.primaryImage.url,
-                caption: result.results.primaryImage.caption.plainText
-            })
-        } catch (error) {
-            console.error(error);
-        }
-    }
-    
         useEffect(() => {
             getMovieData(slug)
-            console.log(imdb)
         },[slug])
 
     return(
         <main>
             <h1>Sjanger: {slug} ({movieList?.size} filmer)</h1>
             <ul>
-                {movieList?.map((movie, index) => <li key={"movie"+index}><MovieCard title={movie.movietitle} image={imdb[index].url} imageText={imdb[index].caption} imdbId={movie.imdbid}/></li>)}
+                {movieList?.map((movie, index) => <li key={"movie"+index}><MovieCard title={movie.movietitle} imdbId={movie.imdbid}/></li>)}
             </ul>
         </main>
     )
--- frontend/src/components/Moviecard.jsx
+++ frontend/src/components/Moviecard.jsx
@@ -1,12 +1,38 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect} from "react";
+import { apiClient } from "../../imdbapi/apiClient";
 
-export default function MovieCard({title, image, imageText, imdbId}) {
+export default function MovieCard({title, imdbId}) {
+
+    const [imdbImage, setImdbImage] = useState()
+
+    const fetchImdbData = async(movieId) => {
+        const url = `https://moviesdatabase.p.rapidapi.com/titles/${movieId}`;
+        try {
+            const response = await fetch(url, apiClient);
+            const result = await response.json();
+            setImdbImage(
+                {
+                    url: result.results.primaryImage.url,
+                    caption: result.results.primaryImage.caption.plainText
+                })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchImdbData(imdbId)
+    },[imdbId])
+
+
     return (
         <article>
             <Link to={`https://www.imdb.com/title/${imdbId}`}>
                 <picture>
-                    <source media="(min-width:300px)" srcset={image}/>
-                    <img src={image} alt={imageText}  width="300"></img>
+                    <source media="(min-width:300px)" srcSet={imdbImage?.url}/>
+                    <img src={imdbImage?.url} alt={imdbImage?.caption}  width="300" height="420"></img>
                 </picture>
             </Link>
             <Link to={`https://www.imdb.com/title/${imdbId}`}>
