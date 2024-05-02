a35cc9c7f24c6afe2396fef5419c42a6209e0d43
M1kke95 <m1kke95@hotmail.com>
2024-05-02 20:39:39 +0200

    fjernet flere imports etc
    var noen imports som ikke ble brukt, samt en prop som ikke ble brukt

--- frontend/src/components/DashMovieCard.jsx
+++ frontend/src/components/DashMovieCard.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react"
-import { apiClient } from "../../imdbapi/apiClient";
+import { useState } from "react"
+
 
 export default function DashMovieCard({movie}) {
 
