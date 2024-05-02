414a49e9f2c6a31baf5f2dbe9ab413c4141187f0
Andreasberg89 <andber9@gmail.com>
2024-05-02 20:12:05 +0200

    moviecard tar nå classname som en parameter

--- frontend/src/components/Moviecard.jsx
+++ frontend/src/components/Moviecard.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useEffect} from "react";
 import { apiClient } from "../../imdbapi/apiClient";
 
-export default function MovieCard({title, imdbId}) {
+export default function MovieCard({title, imdbId, className}) {
 
     const [imdbImage, setImdbImage] = useState()
 
@@ -28,7 +28,7 @@ export default function MovieCard({title, imdbId}) {
 
 
     return (
-        <article>
+        <article htmlClass={className}>
             <Link to={`https://www.imdb.com/title/${imdbId}`}>
                 <picture>
                     <source media="(min-width:300px)" srcSet={imdbImage?.url}/>
