d843c2cc0f2eb5f4c6f477e38a42debe5ba0e7b1
Andreasberg89 <andber9@gmail.com>
2024-05-05 13:36:40 +0200

    Fikset bug der siden krasjet hvis filmer ikke får ett bilde fra movieAPI.

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -7,21 +7,28 @@ export default function MovieCard({movie, className}) {
 
 
     const [imdbImage] = useState({
-        url: movie.primaryImage.url,
-        caption: movie.primaryImage.caption.plainText
+        url: movie.primaryImage?.url,
+        caption: movie.primaryImage?.caption?.plainText
     })
 
 
     return (
         <article className={className}>
-            <Link to={`https://www.imdb.com/title/${movie.id}`}>
+            {(imdbImage.url) ?
+            (<Link to={`https://www.imdb.com/title/${movie?.id}`}>
                 <picture>
                     <source media="(min-width:300px)" srcSet={imdbImage?.url}/>
                     <img src={imdbImage?.url} alt={imdbImage?.caption}  width="300" height="420"></img>
                 </picture>
-            </Link>
-            <Link className="movieCardTitle" to={`https://www.imdb.com/title/${movie.id}`}>
-                {movie.titleText.text}
+            </Link>)
+            :
+            (//Bilde hentet fra: https://www.flaticon.com/free-icon/photo_15402170?related_id=15402170
+            <picture>
+                <source media="(min-width:300px)" srcSet="../../public/ImageNotFound.png"/>
+                <img src={"ImageNotFound.png"} alt="Denne filmen har ikke bilde"  width="300" height="420"></img>
+            </picture>)}
+            <Link className="movieCardTitle" to={`https://www.imdb.com/title/${movie?.id}`}>
+                {movie?.titleText?.text}
             </Link>
         </article>
         
